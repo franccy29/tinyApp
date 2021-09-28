910591a6d3dd69f257103007e6e544c62c39749c
François Martineau <franccy29@gmail.com>
2021-09-28 18:53:18 +0000

    end of day 1

--- express_server.js
+++ express_server.js
@@ -1,16 +1,41 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 const PORT = 8080; // default port 8080
 
-app.set('view engine', 'ejs');
+const generateRandomString = () => {
+  let result           = '';
+  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 5; i++) {
+    result += characters.charAt(Math.floor(Math.random() * 62));
+  }
+  return result;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/urls", (req, res) => {
+  const tinyURL = generateRandomString();
+  urlDatabase[tinyURL] = req.body.longURL;
+  res.render("urls_show", { shortURL: tinyURL, longURL: req.body.longURL });
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls", (req, res) => {
